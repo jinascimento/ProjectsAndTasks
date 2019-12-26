8e922101a1a27b437d2092b4670f1722169f680d
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-12-26 20:43:31 -0300

    Add task CRUD

--- start/routes.js
+++ start/routes.js
@@ -13,6 +13,7 @@ Route.group(() => {
   Route.post('/files', 'FileController.store')
 
   Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects.tasks', 'TaskController').apiOnly()
 }).middleware(['auth'])
 
 
